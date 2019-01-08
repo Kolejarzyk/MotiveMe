052f24f30c5c06b2c1d2281a35c94f02b958f9e7
Kolejarzyk <pawel.dee@live.com>
2019-01-08 18:23:25 +0100

    Add functionality to Note Page

---
+++ MotiveMe/MotiveMe/CreateNotePage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MotiveMe
+{
+
+	public partial class CreateNotePage : ContentPage
+	{
+		public CreateNotePage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void AddNote_Clicked(object sender, EventArgs e)
+        {
+            var note = new Note()
+            {
+                Topic = NoteTitle.Text,
+                Value = Description.Text
+            };
+
+            AppDatabase.noteDao.InsertNote(note);
+            Navigation.PushAsync(new NotePage());
+        }
+    }
+}
---
+++ MotiveMe/MotiveMe/DAO/NoteDAO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MotiveMe.DAO
+{
+   public class NoteDAO
+    {
+        public List<Note> NoteDao;
+
+        public NoteDAO()
+        {
+            NoteDao = new List<Note>();
+        }
+
+
+        public void InsertNote(Note note)
+        {
+            NoteDao.Add(note);
+        }
+
+        public void DeleteNote(int id)
+        {
+            NoteDao.RemoveAt(id);
+        }
+
+
+        public Note GetById(int id)
+        {
+           return NoteDao[id];
+        }
+
+        public List<Note> GetAll()
+        {
+            return NoteDao;
+        }
+    }
+}
