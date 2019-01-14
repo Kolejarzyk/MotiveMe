2a20b1e0fda09b60efb328699c4a76d95f53227c
Kolejarzyk <pawel.dee@live.com>
2019-01-14 21:35:58 +0100

    Add Activity Detail Page with passing data

---
+++ MotiveMe/MotiveMe/ActivityDetailPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using MotiveMe.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MotiveMe
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ActivityDetailPage : ContentPage
+	{
+        public ActivityDetailPage()
+        {
+
+        }
+		public ActivityDetailPage(Activity content)
+		{
+			InitializeComponent ();
+
+            Goal.Text = content.ActivityVal;
+            Date.Text = content.Date.ToString();
+            Reward.Text = content.Reward;
+        }
+	}
+}
