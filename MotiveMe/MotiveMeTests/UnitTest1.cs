d3a401206d5b4b07c750094ee048abd3f52e060c
Kolejarzyk <pawel.dee@live.com>
2019-01-21 20:45:17 +0100

    fixes to test project

---
+++ MotiveMe/MotiveMeTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MotiveMe.DAO;
+
+namespace MotiveMeTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var activity = new ActivityDAO();
+        }
+    }
+}
